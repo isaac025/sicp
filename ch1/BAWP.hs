f1e14c8e427ec7483f24a0e4b4a3c9530be1ee47
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2023-02-19 23:10:15 -0400

    Finished section 1.1

--- ch1/BAWP.hs
+++ ch1/BAWP.hs
@@ -21,7 +21,7 @@ absolute'' x = if x < 0 then (-x)
 infixr 4 >~ 
 
 x >~ y = x > y || x == y
-x >~ y = not (x < y)
+--x >~ y = not (x < y)
 
 -- Exercise 1.1) What is the result printed by the interpreter in response to
 -- each expression? Assume that the sequence is to be evaluated in the order in
@@ -57,4 +57,56 @@ largeSquares x y z
     | y > x && z > x = sumOfSquares y z
     | x > y && z > y = sumOfSquares x z
 
--- Exercise 1.5)
+-- Exercise 1.4) Describe the behavior of the following procedure:
+-- aPlusAbsB a b = (if b > 0 then (+) else (-)) a b
+-- this procedure returns another procedure (either plus or minus) and applies it to a and b
+--
+-- Exercise 1.5) Ben Bitdiddle has invented a test to determine whether the
+-- interpreter he is faced with is using applicative-order evaluation or
+-- normal-order evaluation. He defines the following to procedures:
+  
+-- f = f
+-- test x y = if x == 0 then 0 else y
+  
+-- Then he evaluates the expression: test 0 f
+-- Applicative-order - evaluates the body of test without evaluating f resulting
+-- in 0
+-- Normal-order - evaluates the body of f and infinitely stills in a loop 
+
+sqrtIter guess x = if goodEnough guess x then guess else sqrtIter (improve guess x) x 
+
+improve guess x = average guess (x / guess)
+
+average x y = (x + y) / 2
+
+goodEnough guess x = (abs ((square guess) - x)) < 0.001
+
+sqrt' x = sqrtIter 1.0 x
+
+-- Exercise 1.6) Alyssa P. Hacker doesn't see why if needs to be provided as a
+-- special form. "Why can't I just define it as an ordinary procedure in terms
+-- of cond?" she asks. Alyssa's friend Eva Lu Ator claims that this can indeed
+-- be done, and she defines a new version of if.
+  
+newIf predicate thenClause elseClause
+    | predicate = thenClause
+    | otherwise = elseClause
+
+sqrtIter' guess x = newIf (goodEnough guess x) guess (sqrtIter' (improve guess x) x)
+
+sqrt'' x = sqrtIter' 1.0 x
+-- What happens when Alyssa attempts to use this to compute square roots?
+-- Explain. Nothing??
+--
+-- Exercise 1.7) An alternative strategy for implementing goodEnough is to watch
+-- how guess changes from one iteration to the next and to stop when the change
+-- is a very small fraction of the guess. Design a square-root procedure that
+-- uses this kind of end test. Does this work better for small and bigger
+-- numbers?
+-- goodEnough' guess x
+--
+-- Exercise 1.8) Newton's method for cube roots is based on the fact that if y
+-- is an approximation to the cube root of x, then a better approximation is
+-- given by: (x/y^2 + 2y)/3
+-- Use this formula to implement the cube-root procedure analogous to the
+-- square-root procedure
